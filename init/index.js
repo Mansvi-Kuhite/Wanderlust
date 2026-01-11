6489209ad976c7d55fb24856232394e1764ca8e9
Manasvi Kuhite <manasvikuhite@gmail.com>
2026-01-11 15:27:54 +0530

    Fix MongoDB connection for Render

--- init/index.js
+++ init/index.js
@@ -2,7 +2,8 @@ const  mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGODB_URI;
+
 
 main()
  .then(() => {
@@ -26,5 +27,5 @@ const initDB = async () => {
     console.log("Data initialized");
 };
 
-initDB();
+//initDB();
 
